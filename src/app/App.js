ed343c5f8445be97554be136cff07dca139fcf48
Gabriel Panebianco <gapbriel@gmail.com>
2019-11-18 19:16:35 -0300

    Reorder files with tdd description

---
+++ src/app/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
